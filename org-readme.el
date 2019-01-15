26baaf0364787427da96cc8d8a2d5b1c806d8164
Joe Bloggs <vapniks@yahoo.com>
2019-01-15 10:32:15 +0000

    org-readme-sync: remove redundant check

--- org-readme.el
+++ org-readme.el
@@ -1592,7 +1592,6 @@ When COMMENT-ADDED is non-nil, the comment has been added and the syncing should
 		   (message "Posting Description to emacswiki")
 		   (org-readme-convert-to-emacswiki))))
       (if (and (not comment-added)
-	       (eq major-mode 'emacs-lisp-mode)
 	       (org-readme-check-opt org-readme-update-changelog))
 	  ;; Update the Changelog file if necessary (and set `comment-added' to t)
 	  (progn
